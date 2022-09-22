7960737a1083e1caf7c1b09927edca70d8a99dd4
tigercoin-world <shsfse@naver.com>
2022-09-22 14:53:38 +0900

    initial

---
+++ main.tf
@@ -0,0 +1,11 @@
+# Configure the AWS Provider
+provider "aws" {
+  profile = "wemade"
+  region = "ap-northeast-2"
+}
+
+# Create a VPC
+resource "aws_vpc" "example" {
+  cidr_block = "10.0.0.0/16"
+}
+
